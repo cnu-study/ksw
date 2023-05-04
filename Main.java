a196f7412afef6475bfa873974edff63aa296992
Kosw <swko4512@gmail.com>
2023-05-04 14:50:33 +0900

    java111

--- Main.java
+++ Main.java
@@ -14,11 +14,11 @@ class Rectangle {
         height = d;
     }
 
-    public int area() {
+    int area() {
         return width * height;
     }
 
-    public boolean contains(Rectangle r) {
+    boolean contains(Rectangle r) {
         if (x <= r.x && y <= r.y && x + width >= r.x + r.width && y + height >= r.y + r.height) {
             return true;
         } else {
@@ -27,27 +27,35 @@ class Rectangle {
     }
 
     public void show() {
-        System.out.println("Rectangle: (" + x + ", " + y + ") - " + width + " x " + height);
+        System.out.printf("(%d,%d)에서 크기가 %dx%d인 사각혐",x,y,width,height);
     }
 }
 
 public class Main {
 
     public static void main(String[] args) {
-        Rectangle rect1 = new Rectangle();
-        rect1.setRectangle(2, 2, 8, 7);
-        rect1.show();
-        System.out.println("Area: " + rect1.area());
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = new Rectangle();
+        Rectangle r3 = new Rectangle();
 
-        Rectangle rect2 = new Rectangle();
-        rect2.setRectangle(5, 5, 6, 6);
-        rect2.show();
-        System.out.println("Area: " + rect2.area());
+        r1.setRectangle(2, 2, 8, 7);
+        r1.show();
+        System.out.println("");
 
-        if (rect1.contains(rect2)) {
-            System.out.println("rect1 contains rect2");
+        r2.setRectangle(5, 5, 6, 6);
+        r2.show();
+        System.out.println("");
+
+        r3.setRectangle(1, 1, 10, 10);
+        r3.show();
+        System.out.println("");
+
+        System.out.println("r의 면적은 " + r1.area());
+
+        if(r1.contains(r2)){
+            System.out.println("t는 r을 포함합니다.");
         } else {
-            System.out.println("rect1 does not contain rect2");
+            System.out.println("t는 r을 포함하지않습니다.");
         }
     }
 }
