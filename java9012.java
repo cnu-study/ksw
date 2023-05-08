93558ed0047f2e7816a9d0985e6a8d0c5962c671
Kosw <swko4512@gmail.com>
2023-05-08 11:23:30 +0900

    9012

--- java9012.java
+++ java9012.java
@@ -2,24 +2,30 @@ import java.util.*;
 public class java9012 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         int n = in.nextInt();
 
         for (int i = 0; i < n; i++) {
+            Stack<Character> stack = new Stack<>();
             String bae = in.next();
-            int cnt = 0;
+
             for (int j = 0; j < bae.length(); j++) {
                 char c = bae.charAt(j);
+
                 if (c == '(') {
-                    cnt = cnt + 1;
+                    stack.push(c);
                 } else if (c == ')') {
-                    cnt = cnt - 1;
+                    if (stack.empty()) {
+                        System.out.println("NO");
+                        break;
+                    }
+                    stack.pop();
                 }
             }
 
-            if(cnt == 0){
+            if (stack.empty()) {
                 System.out.println("YES");
-            }
-            else{
+            } else {
                 System.out.println("NO");
             }
         }
