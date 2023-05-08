3f511fe0a6a29ff0be4cf9d0f1acf17a13204287
Kosw <swko4512@gmail.com>
2023-05-08 10:33:40 +0900

    c

--- java10773.java
+++ java10773.java
@@ -6,26 +6,25 @@ public class java10773 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         int cnt = 0;
+
         int n = in.nextInt();
 
         for (int i = 0; i < n; i++) {
             int k = in.nextInt();
-            if (k != 0){
+            if(k==0){
+                if(stack.size() != 0){
+                    cnt -= stack.peek();
+                    stack.pop();
+                }
+                else{
+                    cnt -= 0;
+                }
+            }
+            else{
                 stack.push(k);
                 cnt += stack.peek();
-            }
-            else if(k == 0){
-
-                if(stack.size() != 0)
-                {
-                    stack.pop();
-                    if(stack.size() != 0) {
-                        cnt -= stack.peek();
-                    } else {
-                        cnt -= 0;
-                    }
-                }
             }
 
         }
