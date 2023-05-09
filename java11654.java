25079c38726fc5579d0a5852062c07ede249443f
Kosw <swko4512@gmail.com>
2023-05-10 03:10:12 +0900

    211

---
+++ java11654.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class java11654 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println("a");
+    }
+}
+ 
