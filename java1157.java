292c7d0913db3043117c9dc07d734ef830fa58c8
Kosw <swko4512@gmail.com>
2023-05-12 01:45:31 +0900

    c

--- java1157.java
+++ java1157.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class java1157 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int[] arr = new int[26];
