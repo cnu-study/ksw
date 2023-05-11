aedd250e8e9d59aafd7e96d6841ca1a791fd3a3d
Kosw <swko4512@gmail.com>
2023-05-11 16:48:37 +0900

    2차원 배열

---
+++ java11660.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class java11660 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[][] arr = new int[n][n];
+        int[][] bae = new int[4][k];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                bae[i][j] = in.nextInt();
+            }
+        }
+
+
+
+    }
+}
