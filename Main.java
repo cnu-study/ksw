a77b6ee0edca5f82871696f8d8d0eeccb7d709de
Kosw <swko4512@gmail.com>
2023-05-13 12:31:27 +0900

    main

--- Main.java
+++ Main.java
@@ -2,23 +2,5 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-            int a = i;
-            int b = i;
-
-            while (b > 0) {
-                a += b % 10;
-                b /= 10;
-            }
-
-            if (a == n) {
-                System.out.println(i);
-                return;
-            }
-        }
-
-        System.out.println("0");
     }
 }
