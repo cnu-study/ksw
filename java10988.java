605b4573a5003d984cbec284b3d56e9d06c0d3ae
Kosw <swko4512@gmail.com>
2023-05-13 13:52:20 +0900

    앞뒤똑같은 펠린드롬

---
+++ java10988.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class java10988 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String a=in.next();
+        int mid=(a.length()/2);
+        int cnt=1;
+        for(int i=0;i<mid;i++) {
+            if(a.charAt(i)!=a.charAt(a.length()-1-i)) {
+                cnt=0;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
