fe2aae17e109933c2f9b8d06720720befdf28866
Kosw <swko4512@gmail.com>
2023-05-13 12:38:12 +0900

    별찍기

--- java2444.java
+++ java2444.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class java2444 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
