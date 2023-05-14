5e89e246539e8424b4d08ab2e409d2cedba920d8
Kosw <swko4512@gmail.com>
2023-05-14 17:29:20 +0900

    브ㄹ투포스

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        System.out.println("hello");
+        Scanner in = new Scanner(System.in);
+        
     }
 }
