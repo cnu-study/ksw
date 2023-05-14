bc0ad672987f95cb42546b8a2cbb172509803230
Kosw <swko4512@gmail.com>
2023-05-14 20:51:19 +0900

    11866 큐문제 다시보기

--- java11866.java
+++ java11866.java
@@ -1,33 +1,33 @@
 import java.util.*;
-public class java11866 {
+public class java11866
+{
     public static void main(String[] args) {
-        Deque<Integer> deque = new LinkedList<>();
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int k = in.nextInt();
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            deque.addFirst(i+1);
-        }
-        while (true){
-            if(deque.size() == 1)
-            {
-                System.out.println(deque.peek());
-                break;
-            }
-            else{
-                cnt++;
-                if(cnt % k == 0){
-                    deque.addLast(deque.getFirst());
-                    deque.remove();
-                    System.out.println(deque.peek());
-                }
-                else{
-                    deque.addLast(deque.getFirst());
-                    deque.remove();
-                }
-            }
-        }
+		Queue<Integer> q = new LinkedList<>();
+		
+		int N = in.nextInt();
+		int K = in.nextInt();
+		
+		
+		for(int i = 1; i <= N; i++) {
+			q.add(i);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append('<');
+	
+		while(q.size() > 1) {
+			
+			for(int i = 0; i < K - 1; i++) {
+				int val = q.poll();
+				q.offer(val);
+			}
+			
+			sb.append(q.poll()).append(", ");
+		}
+ 
+		// 마지막 원소 출력한 뒤 > 도 붙여준다.
+		sb.append(q.poll()).append('>');
+		System.out.println(sb);
     }
 }
-
