ea1426d0850c8851a755852dde23e637a4d9d08d
Kosw <swko4512@gmail.com>
2023-05-15 15:16:45 +0900

    자바 재귀 함수

--- java10872.java
+++ java10872.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class java10872 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int f = in.nextInt();
