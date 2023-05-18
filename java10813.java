c88b56ef7ceb813e51e2311b0697052b5e5dec63
Kosw <swko4512@gmail.com>
2023-05-18 16:06:24 +0900

    c

--- java10813.java
+++ java10813.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class java10813 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -17,7 +17,7 @@ public class Main {
             tmp = arr[a-1];
             arr[a-1] = arr[b-1];
             arr[b-1] = tmp;
-            }
+        }
 
 
         for (int i = 0; i < n; i++) {
