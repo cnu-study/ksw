256512b494e088dbd3a6285d3eb11666e8ebf159
Kosw <swko4512@gmail.com>
2023-05-20 14:42:13 +0900

    split

--- java27866.java
+++ java27866.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 
 public class java27866 {
     public static void main(String[] args) {
-
-
-
+        Scanner in = new Scanner(System.in);
         String[] a = in.next().split("");
         int b = in.nextInt();
 
