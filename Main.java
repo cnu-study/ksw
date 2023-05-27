805d2289d8891c038f8fdfb1f10b2f459a13e072
kosw <swko4512@gmail.com>
2023-05-27 11:57:04 +0900

    test

--- Main.java
+++ Main.java
@@ -3,31 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Stack<Integer>[] stack = new Stack[7];
-        int n = in.nextInt();
-        int cnt = 1;
-        for (int i = 1; i <= 6; i++) {
-            stack[i] = new Stack<Integer>();
-            stack[i].push(0);
-        }
-
-        stack[in.nextInt()].push(in.nextInt());
-
-        for (int i = 1; i < n; i++) {
-            int ln = in.nextInt();
-            int fn = in.nextInt();
-
-            while (stack[ln].peek() > fn) {
-                stack[ln].pop();
-                cnt++;
-            }
-
-            if (stack[ln].peek() != fn) {
-                stack[ln].push(fn);
-                cnt++;
-            }
-        }
-        System.out.println(cnt);
+        int a = in.nextInt();
+        System.out.println(a);    
     }
-
 }
