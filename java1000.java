31f9b81bd5414d206089bf2f6b0ae066a6d121f9
kosw <swko4512@gmail.com>
2023-05-27 12:00:58 +0900

    fix

--- java1000.java
+++ java1000.java
@@ -7,7 +7,6 @@ public class java1000
         int A = ac.nextInt();
         int B = ac.nextInt();
         System.out.println(A+B);
-        ac.close();
-        
+        ac.close();        
     }
 }
