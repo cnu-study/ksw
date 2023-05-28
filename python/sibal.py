a0f88e774fefc265d62b8e1abf78638999766152
kosw <swko4512@gmail.com>
2023-05-28 11:31:14 +0900

    파이썬 연습

---
+++ python/sibal.py
@@ -0,0 +1,43 @@
+# a = "Life is too short, You need Python"
+# print(a[10:-10])
+# print("I eat %s apples." % "five")
+
+# number = 10
+# day = "three"
+# print("I ate %d apples. so I was sick for %s days." % (number, day))
+
+# print("%5sjune" % "hi")
+# print("i eat {0} apple".format(123))
+# print(a.count('L'))
+# print(a.find('t'))
+# a = " a b c d "
+# print(".".join(a))
+# print(a.upper())
+# print(a.lstrip())
+# print(a.strip())
+# print(a.replace("a b","c d"))
+# print(a.split(","))
+# a = [1, 2, 3, ['a', 'b', 'c']]
+# print(a[-1][1])
+# print(a[0:2])
+
+# a = [1, 2, 3,4]
+# print(str(a[2]) + "hi")
+# a.append([5,6])
+# print(a)
+# a.remove(3)
+# a.pop()
+# a.count(1)
+# a.extend([4,5])
+
+# dic = {'name':'pey', 'phone':'010-9999-1234', 'birth': '1118'}
+# dic[2] = '3'
+# print(dic)
+# del dic[2]
+# print(dic)
+# print(dic['birth'])
+# print(dic.keys())
+# print(dic.values())
+# print(dic.clear())
+# print(dic.get('no','yes'))
+
